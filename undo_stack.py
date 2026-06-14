7b55aab079dc67ecc380b812ca2812c17a5da792
MaryanaAnt <maryanaant2007@gmail.com>
2026-06-14 18:18:53 +0300

    Update undo_stack.py

--- undo_stack.py
+++ undo_stack.py
@@ -1,16 +1,23 @@
-class UndoStack: # принцип LIFO
-    def __init__(self): # инициализация работы стека
+class UndoStack: 
+    """
+    Стек, поддерживающий отмену действий. 
+    Работает по принципу LIFO (последний пришёл - первым ушёл).
+    """
+    # Инициализация работы стека
+    def __init__(self): 
         self.stack = []
 
-    def push(self, action): # добавление задачи
+    # Добавление задачи
+    def push(self, action): 
         self.stack.append(action)
 
-    def pop(self): # удаление и возвращение задачи
-
+    # Удаление и возвращение задачи
+    def pop(self): 
         if len(self.stack) == 0:
             return None
 
         return self.stack.pop()
 
-    def is_empty(self): # проверка на наличие задач в стеке
+    # Проверка на наличие задач в стеке
+    def is_empty(self): 
         return len(self.stack) == 0
